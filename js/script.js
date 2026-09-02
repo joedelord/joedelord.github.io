a2b186f74c0a405bb4d435ec0c07f92ffcb27141
Jouni <83824082+joedelord@users.noreply.github.com>
2026-09-02 14:57:24 +0300

    Refactor CSS variables and style hamburger menu

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const links = document.querySelectorAll(".nav-links a");
 navToggle.addEventListener("click", () => {
   const isOpen = navLinks.classList.toggle("active");
 
+  navToggle.classList.toggle("active");
   navToggle.setAttribute("aria-expanded", isOpen);
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("active");
+    navToggle.classList.remove("active");
     navToggle.setAttribute("aria-expanded", "false");
   });
 });
